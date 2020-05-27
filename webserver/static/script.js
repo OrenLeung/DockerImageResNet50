a9be08331883b3e7ba9d22f53d47d681b4e7c847
OrenLeung <oren.leung@gmail.com>
2020-05-27 13:54:04 -0400

    simple webpage

---
+++ webserver/static/script.js
@@ -0,0 +1,62 @@
+async function updatePredTable(preds) {
+	const header = [ 'Label', 'Predictions %' ];
+
+	let table = document.createElement('table');
+
+	var tr = table.insertRow(-1);
+
+	header.forEach((head) => {
+		var th = document.createElement('th'); // TABLE HEADER.
+		th.innerHTML = head;
+		tr.appendChild(th);
+	});
+
+	preds.forEach((pred) => {
+		tr = table.insertRow(-1);
+
+		var tabCell = tr.insertCell(-1);
+		tabCell.innerHTML = pred['label'];
+
+		var predLabel = tr.insertCell(-1);
+		predLabel.innerHTML = Math.round(pred['probability'] * 10000) / 100;
+	});
+
+	const divContainer = document.getElementById('pred-table');
+	divContainer.innerHTML = '';
+	divContainer.appendChild(table);
+}
+
+async function addImage(file) {
+	let imgDiv = document.getElementById('img-div');
+	imgDiv.innerHTML = '';
+
+	var img = document.createElement('img');
+	img.src = URL.createObjectURL(file);
+	imgDiv.append(img);
+}
+
+async function postResult(file) {
+	const formData = new FormData();
+	formData.append('img_file', file);
+	let results = await axios
+		.post(`/predict`, formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+		.catch((error) => {
+			console.log('bad');
+		});
+
+	return results['data']['predictions'];
+}
+
+document.getElementById('btn').addEventListener('click', async function() {
+	let file = document.getElementById('file-input').files[0];
+
+	addImage(file);
+
+	const preds = await postResult(file);
+
+	updatePredTable(preds);
+});
